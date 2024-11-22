aa2206c530e7c4da25cac56371992cd420f44740
spandana <morampudispandana058@gmail.com>
2024-11-22 17:55:12 -0600

    changes both frontend and backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import {BrowserRoute, Route , Switch , Redirect} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Cart from "./components/Cart";
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
 
 function App() {
+  const shouldRedirect = true; 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/cart" exact element={<Cart />} />
+        <Route path="/not-found" element={<NotFound />} />
+        <Route path="/" exact element={<Home />} />
+        <Route path="*" element={<Navigate to="/not-found" replace />} />
+      </Routes>
+     
     </div>
   );
 }
